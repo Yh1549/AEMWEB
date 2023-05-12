aee6855bf1230d2b970c3a5be3b1565246a9761f
鄭翊宏 <>
2023-05-12 09:50:03 +0800

    update

--- src/api/apiRequest.js
+++ src/api/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Store } from "../store/store";
 import { commonStore } from "../store/commonStore";
-import router from "../router/router.js"
+import router from "../router/router.js";
 export const apiRequest = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   timeout: 10000,
@@ -12,7 +12,9 @@ export const apiRequest = axios.create({
 // 添加請求攔截器  global
 apiRequest.interceptors.request.use(
   (config) => {
-    config.headers['Authorization'] = "Bearer " + sessionStorage.getItem('token');
+    config.url += ".json";
+    config.headers["Authorization"] =
+      "Bearer " + sessionStorage.getItem("token");
     return config;
   },
   (error) => {
@@ -22,22 +24,22 @@ apiRequest.interceptors.request.use(
 //response
 apiRequest.interceptors.response.use(
   async (res) => {
-    if (res.data.desc != 'successful') {
+    if (res.data.desc != "successful") {
       if (commonStore().loginErrorHandle(res.data)) {
         Store().alertShow = true;
         Store().alertObj = {
           msg: `錯誤，${res.data.desc}`,
           func: (e) => {
             commonStore().tokenRemove();
-          }
-        }
-      };
+          },
+        };
+      }
       return res.data;
     } else {
       if (res.headers["token"]) {
         sessionStorage.setItem("token", res.headers["token"]);
       }
-      return res.data
+      return res.data;
     }
   },
   function (error) {
@@ -48,32 +50,30 @@ apiRequest.interceptors.response.use(
       Store().alertShow = true;
       Store().alertShow = {
         msg: `連線超時，請稍後在試`,
-        func: (e) => {
-
-        },
+        func: (e) => {},
       };
       console.log("連線超時");
     }
     if (error.response) {
       switch (error.response.status) {
         case 404:
-          router.push({ name: '404error' })
-          commonStore().apiErrorMsg = '你要找的頁面不存在';
+          router.push({ name: "404error" });
+          commonStore().apiErrorMsg = "你要找的頁面不存在";
           console.log("404：Not Found 你要找的頁面不存在");
           break;
         case 500:
-          router.push({ name: '404error' })
-          commonStore().apiErrorMsg = 'Internal Server Error';
+          router.push({ name: "404error" });
+          commonStore().apiErrorMsg = "Internal Server Error";
           console.log("500：Internal Server Error 程式發生問題");
           break;
         case 502:
-          router.push({ name: '404error' })
-          commonStore().apiErrorMsg = 'Bad Gateway';
+          router.push({ name: "404error" });
+          commonStore().apiErrorMsg = "Bad Gateway";
           console.log("502：Bad Gateway 上游伺服器接收到無效的回應");
           break;
         case 504:
-          router.push({ name: '404error' })
-          commonStore().apiErrorMsg = 'Gateway Timeout';
+          router.push({ name: "404error" });
+          commonStore().apiErrorMsg = "Gateway Timeout";
           console.log(
             "504：Gateway Timeout 未能及時從上游伺服器或者輔助伺服器收到回應"
           );
@@ -82,7 +82,7 @@ apiRequest.interceptors.response.use(
           console.log("ERROR 200");
           break;
         default:
-          router.push({ name: '404error' })
+          router.push({ name: "404error" });
           console.log(error.message);
       }
     }
